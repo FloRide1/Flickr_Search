0266204e901ac1e153d7ed1bb33510a39221ddd8
FloRide1 <florian.reimat@orange.fr>
2022-06-26 16:24:24 +0200

    [SERV] Add search_keyword function

--- src/app/services/flickr.service.ts
+++ src/app/services/flickr.service.ts
@@ -24,4 +24,22 @@ export class FlickrService
 {
     constructor(private http: HttpClient)
     {}
+
+    search_keyword(keyword: string)
+    {
+        const url = "www.flickr.com/services/rest/?method=flickr.text.echo&name=value";
+        const params = `api_key=${environment.flickr.key}&text=${keyword}&format=json&nojsoncallback=1&per_page=12`;
+
+        return this.http.get(url + params).pipe(map((res: FlickrOutput) => {
+            const urlArr: any = [];
+            res.photos.photo.forEach((ph: FlickrPhoto) => {
+                const photoObj = {
+                    url: `farm${ph.farm}.staticflickr.com/${ph.server}/${ph.id}_${ph.secret}`,
+                    title: ph.title
+                };
+                urlArr.push(photoObj);
+            });
+            return urlArr;
+        }));
+    }
 }
