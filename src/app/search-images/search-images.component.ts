d6885ef24f66cd40805832dcff50b3ff2fb37861
FloRide1 <florian.reimat@orange.fr>
2022-07-03 17:04:00 +0200

    [COMP] Create search function inside search-image component

--- src/app/search-images/search-images.component.ts
+++ src/app/search-images/search-images.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component,
+        OnInit} from '@angular/core';
+
+import {FlickrService} from '../services/flickr.service';
 
 @Component({
-  selector: 'app-search-images',
-  templateUrl: './search-images.component.html',
-  styleUrls: ['./search-images.component.css']
+    selector: 'app-search-images',
+    templateUrl: './search-images.component.html',
+    styleUrls: ['./search-images.component.css']
 })
-export class SearchImagesComponent implements OnInit {
+export class SearchImagesComponent implements OnInit
+{
+    images: any = [];
+    keyword: string = "";
+    constructor(private flickrService: FlickrService)
+    {}
 
-  constructor() { }
+    ngOnInit(): void
+    {
+    }
 
-  ngOnInit(): void {
-  }
-
+    search(event: any)
+    {
+        this.keyword = event.target.value.toLowerCase();
+        if (this.keyword && this.keyword.length > 0)
+        {
+            this.flickrService.search_keyword(this.keyword)
+                .toPromise()
+                .then(res => {
+                    this.images = res;
+                });
+        }
+    }
 }
