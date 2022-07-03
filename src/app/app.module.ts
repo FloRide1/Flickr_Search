b367b294c91ad4611bf8eb72ca6ed2d799eeff6b
FloRide1 <florian.reimat@orange.fr>
2022-07-03 19:02:09 +0200

    [APP] Add angular modules in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import {BrowserModule} from '@angular/platform-browser';
+//
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 import {SearchImagesComponent} from './search-images/search-images.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -15,6 +23,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         BrowserModule,
         HttpClientModule,
         BrowserAnimationsModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatDatepickerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatNativeDateModule,
+        MatCheckboxModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
